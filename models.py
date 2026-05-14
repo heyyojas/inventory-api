369d48d0a7fd31f322ff03e8a2ff7040c6e46043
ojas sharma <sharmaojas56@gmail.com>
2026-05-14 15:30:48 +0530

    deployment changes

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-
 from pydantic import BaseModel
 
-class Products(BaseModel): 
-    id : int
-    name : str
-    description : str
-    price : float
-    quantity : int
+class Products(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    class Config:
+        orm_mode = True
