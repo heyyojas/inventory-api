fa5b3ba811df635eee0b228680c816646551f242
ojas sharma <sharmaojas56@gmail.com>
2026-05-14 14:58:38 +0530

    added database

--- database.py
+++ database.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+db_url = os.environ.get("DATABASE_URL")
+engine = create_engine(db_url)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 db_url = "postgresql://postgres:ojasman%40%231109@localhost:5432/telusko"
 engine = create_engine(db_url)
 session = sessionmaker(autocommit = False, autoflush=False, bind = engine)
